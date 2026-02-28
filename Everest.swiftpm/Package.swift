11fe5680dc77368c835596e5632945448d7a7e7c
Ethan Leonard <ethan.leonard821@gmail.com>
2026-02-28 15:01:34 -0800

    Fix Package.swift for Xcode compatibility

--- Everest.swiftpm/Package.swift
+++ Everest.swiftpm/Package.swift
@@ -4,31 +4,12 @@ import PackageDescription
 let package = Package(
     name: "Everest",
     platforms: [
-        .iOS("17.0")
+        .iOS(.v17)
     ],
     products: [
-        .iOSApplication(
+        .library(
             name: "Everest",
-            targets: ["AppModule"],
-            bundleIdentifier: "com.everest.playground",
-            teamIdentifier: "",
-            displayVersion: "1.0",
-            bundleVersion: "1",
-            appIcon: .placeholder(icon: .mountain),
-            accentColor: .presetColor(.green),
-            supportedDeviceFamilies: [
-                .pad,
-                .phone
-            ],
-            supportedInterfaceOrientations: [
-                .portrait,
-                .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
-            ],
-            capabilities: [
-                .localNetwork(purposeString: "Local Network Not Used By Everest Challenge App", bonjourServiceTypes: [])
-            ]
+            targets: ["AppModule"]
         )
     ],
     targets: [
